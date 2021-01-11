9698b65ebd4a1204e92e2e152346d6dd45ac5399
Marina Kosolap <70973716+Luna-na-na@users.noreply.github.com>
2021-01-11 22:45:17 +0700

    Create main.cpp

---
+++ hash_t/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "HashTable.h"
+#include <gtest/gtest.h>
+#include "test.cpp"
+
+using namespace std;
+
+
+int main() {
+
+    testing::InitGoogleTest();
+    RUN_ALL_TESTS();
+
+
+    cout<<"end";
+    return 0;
+}
+
